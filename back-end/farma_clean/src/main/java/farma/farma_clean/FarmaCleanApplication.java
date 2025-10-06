180700f34519a84cd7a852f235c4b44e6a0716ea
Caio Riciati <esdrascaio5313@gmail.com>
2025-10-06 07:46:56 -0300

    criando back-end

---
+++ back-end/farma_clean/src/main/java/farma/farma_clean/FarmaCleanApplication.java
@@ -0,0 +1,13 @@
+package farma.farma_clean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaCleanApplication.class, args);
+	}
+
+}
---
+++ back-end/farma_clean/src/test/java/farma/farma_clean/FarmaCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package farma.farma_clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
